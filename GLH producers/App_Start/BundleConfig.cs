3752385cb24b07047092886e8194a00a922cb5b3
JamieRIna <jamie.rina.icloud.com>
2026-04-23 18:13:38 +0100

    Refresh GLH Producers UI styling

--- GLH producers/App_Start/BundleConfig.cs
+++ GLH producers/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace GLH_producers
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
